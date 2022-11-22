bf1f71751abba8d65cdc4b456da2d5dfa792e063
Muhammad Mubashir Mughal <donleo26@gmail.com>
2022-11-21 21:53:12 -0500

    sdfas

--- src/Main.java
+++ src/Main.java
@@ -1,5 +1,35 @@
+abstract class ArithmeticExp {
+    private int type;
+    private int value1;
+    private int value2;
+    private ArithmeticExp left;
+    private ArithmeticExp right;
+
+}
+class number extends ArithmeticExp{
+
+
+}
+class sum extends ArithmeticExp{
+
+}
+class product extends ArithmeticExp{
+
+}
+
+abstract class binary {
+
+}
+
+
 public class Main {
-    public static void main(String[] args) {
-        System.out.println("Hello world!");
+    public static void main(String[] args)
+    {
+        ArithmeticExp term = new ArithmeticExp(TYPE_SUM, 0,
+                new ArithmeticExp(TYPE_NUMBER, 3, null, null),
+                new ArithmeticExp(TYPE_PROD, 0,
+                        new ArithmeticExp(TYPE_NUMBER, 2, null, null),
+                        new ArithmeticExp(TYPE_NUMBER, 5, null, null)));
+        System.out.println(evaluate(term));
     }
 }
